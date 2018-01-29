c8d5fa2b5d99c910bcdb6b9f1ff770e73a448e37
Sierra McCahon <smitche4@mail.umw.edu>
2018-01-29 21:00:06 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -39,5 +39,13 @@ def draw?(board)
   end
 end
 
-def over?(board)
+def over?(array3)
+  if full?(array3) || !(won?(array3)) || draw?(array3)
+    puts "GAME OVER"
+    return true
+  else 
+    puts "it ain't over till it's over baby!"
+    return false 
+  end 
+end  
   
